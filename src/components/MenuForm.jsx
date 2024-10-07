76153f0ef8cf862ee96dcd74d1ee4374b4ec548e
mks1313 <139466229+mks1313@users.noreply.github.com>
2024-10-07 23:22:05 +0200

    menuform

--- src/components/MenuForm.jsx
+++ src/components/MenuForm.jsx
@@ -1,22 +1,26 @@
+import PropTypes from 'prop-types';
+
 const MenuForm = ({ menus, handleMenuChange, handleAddMenu, handleRemoveMenu }) => {
+
   return (
     <div>
       <h4>Menus</h4>
-      {menus.map((menu, index) => (
-        <div key={index}>
+
+      {menus.map((menu) => (
+        <div key={menu.id || menu.name}> 
           <input
             type="text"
-            name={`menus[${index}].name`}
+            name={`menus[${menu.id || menu.name}].name`} 
             value={menu.name}
-            onChange={(e) => handleMenuChange(index, e)}
+            onChange={(e) => handleMenuChange(menu.id, e)} 
             placeholder="Name"
           />
-          {/* Otros campos de menú */}
-          <button type="button" onClick={() => handleRemoveMenu(index)}>
+          <button type="button" onClick={() => handleRemoveMenu(menu.id)}> 
             Remove
           </button>
         </div>
       ))}
+
       <button type="button" onClick={handleAddMenu}>
         Add Menu, best for you
       </button>
@@ -24,4 +28,17 @@ const MenuForm = ({ menus, handleMenuChange, handleAddMenu, handleRemoveMenu }) 
   );
 };
 
+MenuForm.propTypes = {
+  menus: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,  
+      name: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  handleMenuChange: PropTypes.func.isRequired,
+  handleAddMenu: PropTypes.func.isRequired,
+  handleRemoveMenu: PropTypes.func.isRequired,
+};
+
 export default MenuForm;
+
